f3d7be48d2304d8bdd7662bcbc905e8c98b2c3c7
18869978285 <18869978285@163.com>
2018-02-08 18:28:22 +0800

    Initial commit

---
+++ app/src/main/java/axun/com/viewpagerbanner/MainActivity.java
@@ -0,0 +1,13 @@
+package axun.com.viewpagerbanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ banner/src/main/java/axun/com/banner/Banner.java
@@ -0,0 +1,26 @@
+package axun.com.banner;
+
+import android.content.Context;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Administrator on 2018/2/8.
+ */
+
+public class Banner extends FrameLayout {
+    public Banner(@NonNull Context context) {
+        this(context,null);
+    }
+
+    public Banner(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public Banner(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
